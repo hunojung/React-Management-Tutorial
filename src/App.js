a711957fe584ccee1efb49d945991bd4b66fc2de
hunojung <huno0504@naver.com>
2022-03-01 22:14:30 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,41 @@ import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
 
-const customer = {
-  'name' : 'hunojung',
+const customers = [{
+  
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name' : 'jho',
   'birthday' : '920504',
   'gender' : '남자',
   'job' : '대학생'
-}
 
+},{
+
+  'id':1,
+  'image':'https://placeimg.com/64/64/2',
+  'name' : 'kkh',
+  'birthday' : '960504',
+  'gender' : '남자',
+  'job' : '대학생'
+
+},{
+
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name' : 'jhj',
+  'birthday' : '960504',
+  'gender' : '남자',
+  'job' : '대학생'
+
+}]
 function App() {
   return (
-    <Customer 
-      name = {customer.name}
-      birthday = {customer.birthday}
-      gender = {customer.gender}
-      job = {customer.job}/>
-
+    <div>
+      {customers.map(c=>{
+        // map을 쓰면 원소를 구분할 수 있는 key를 넣어줘야한다.
+        return(  <Customer key={c.id} id = {c.id}  image = {c.image} name = {c.name} birthday = {c.birthday}  gender = {c.gender} job = {c.job}/> ) })}
+      </div>
   );
 }
 
